5daf26da963fdbc09d49db3df4ec60871a0197c8
Edwinliby <edwinliby30@gmail.com>
2024-04-02 14:19:46 +0530

    ..

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -8,7 +8,7 @@ const alertVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "bg-[#FF471F] text-white",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
